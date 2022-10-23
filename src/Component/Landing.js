35962a725c190436426a4e9adcfdb5c632ece375
18-09-1980_sep <mernosen@gmail.com>
2022-10-23 20:30:30 +0100

    built and styled landing and user components

---
+++ src/Component/Landing.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Landing.css";
+import { Link } from "react-router-dom";
+function Landing() {
+    return (
+
+        <>
+      <div className="main-container">
+        <h1 className="header-text">Login to See Random Users</h1>
+        <div className="formContainer">
+          <input type="text" name="email" id="email" placeholder="Email" />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <link to="/User" className="btn-submit" type="submit">
+            Sign In
+          </link>
+        </div>
+      </div>
+    </>
+);
+}
+ export default Landing;
---
+++ src/Component/User.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Landing (){
+  return (
+    <div>
+      <div> className = main-user-container</div>
+      <h1 className="title-heading">HELLO, I AM A NEW USER 🧑</h1>
+<div>user-box
+<img
+            className="user-image"
+            src="https://randomuser.me/api/portraits/women/3.jpg"
+            alt="User-profile"
+          />
+          <div className="user-info">
+            <h2>Name: Karl Johnson </h2>
+            <h2>Email: karl.johnson@example.com</h2>
+            <h2>Birthday: 1966-12-17 </h2>
+            <h2>Address: 6057 Avondale Ave</h2>
+            <h2>Phone-No: (268) 420-4900</h2>
+            <h2>Password: luan </h2>
+          </div>
+
+          <div className="buttons">
+          <button className="prev">PREV</button>
+          <div className="number-buttons">
+            <button className="num">1</button>
+            <button className="num">2</button>
+            <button className="num">3</button>
+            <button className="num">4</button>
+            <button className="num">5</button>
+          </div>
+          <button className="next">NEXT</button>
+        </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Landing;
